5f55d49246ab73b6d8d24fc0988f4103b2aebecf
AbhilashRPrasad <108757229+AbhilashRPrasad@users.noreply.github.com>
2022-11-01 19:43:26 +0530

    NextAuth imported

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyD-kFsv-dByrySLArQAi5COeeWYAKgK170",
+  authDomain: "amzn-2-arp.firebaseapp.com",
+  projectId: "amzn-2-arp",
+  storageBucket: "amzn-2-arp.appspot.com",
+  messagingSenderId: "271515757995",
+  appId: "1:271515757995:web:8d425f1b4577c2201736ee"
+};
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ import {
     SearchIcon,
     ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import {signIn, signOut, useSession} from 'next-auth/client';
 
 export default function Header() {
     return (
@@ -28,7 +29,7 @@ export default function Header() {
                 </div>
                 {/* Right side */}
                 <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-                    <div className='link'>
+                    <div onClick= {signIn} className='link'>
                         <p>Hello Abhilash R</p>
                         <p className='font-extrabold md:text-sm'>Account & Lists</p>
                     </div>
