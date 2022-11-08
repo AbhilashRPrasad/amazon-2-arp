b7b0add2eb8a04de12d0f700287adcf6aa78cdad
AbhilashRPrasad <108757229+AbhilashRPrasad@users.noreply.github.com>
2022-11-08 23:18:49 +0530

    stripe added success

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -2,36 +2,36 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 export default async (req, res) => {
   const { items, email } = req.body;
   const transformedItems = items.map((item) => ({
-    description: item.description,
     quantity: 1,
     price_data: {
       currency: "INR",
       unit_amount: item.price * 100,
       product_data: {
         name: item.title,
+        description: item.description,
         images: [item.image],
       },
     },
   })); //Implicit return
 
-  console.log(items);
-  console.log(email);
-  console.log(transformedItems);
+  // console.log(items);
+  // console.log(email);
+  // console.log(transformedItems);
 
   const session = await stripe.checkout.sessions.create({
-    // payment_method_types: ["card"],
-    // shipping_options: ["shr_1M1onWSJfPsQvTzGMvBcfBzi"],
-    // shipping_address_collection: {
-    //   allowed_countries: ["IN", "US", "CA"],
-    // },
+    payment_method_types: ["card"],
+    shipping_options: [{ shipping_rate: "shr_1M1onWSJfPsQvTzGMvBcfBzi" }],
+    shipping_address_collection: {
+      allowed_countries: ["IN", "US", "CA"],
+    },
     line_items: transformedItems,
     mode: "payment",
     success_url: `${process.env.HOST}/success`,
     cancel_url: `${process.env.HOST}/checkout`,
-    // metadata: {
-    //   email,
-    //   images: JSON.stringify(items.map((item) => item.image)),
-    // },
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
   });
 
   res.status(200).json({ id: session.id });
